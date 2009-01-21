ba3539bc0c665387c256439b65a9d21af8d99017
Bernd Wachter <bwachter-awfulcms@lart.info>
2009-01-21 20:50:36 +0100

    added obsolete ttf.pl for historical reasons

---
+++ ttf.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+use lib "/home/bwachter/aard";
+use strict;
+use AwfulCMS::Page;
+
+my $request="default";
+my $module;
+my $call;
+my $s={};
+my $m; #AwfulCMS::ModExample->new($s);
+
+# set up a page for later use and find out about module and request foo
+my $p=AwfulCMS::Page->new("test");
+
+if (defined($p->{rq_vars}->{req})){
+  $request=$p->{rq_vars}->{req};
+}
+
+if (defined($p->{rq_vars}->{mod})){
+  # module validation, whatever
+} else {
+
+}
+
+$p->{rq_file}="test.html";
+
+$module="AwfulCMS::ModTemplate";
+eval "require $module";
+$p->s404($@) if ($@);
+
+$m=$module->new($s, $p);
+$p->s404("Unable to load module") if (ref($m) ne $module);
+
+
+unless (defined $s->{rqmap}->{$request}->{-handler}){
+  if (defined $s->{rqmap}->{default}->{-handler}){
+    $request="default";
+  } else {
+    $p->s404("No default method defined in module");
+  }
+}
+
+if (defined $s->{rqmap}->{$request}->{-dbrw}){
+  # open rw database connection
+} elsif (defined $s->{rqmap}->{$request}->{-dbro}){
+  # open ro database connection
+}
+
+$call=$s->{rqmap}->{$request}->{-handler};
+$m->$call();
+
+$p->out();
