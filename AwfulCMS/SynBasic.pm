c842bf06568b156c45651dd059b3c2bf36e4ccb0
Bernd Wachter <bwachter-awfulcms@lart.info>
2009-10-18 02:29:57 +0200

    changed matching to lookahead to get all paragraphs

--- AwfulCMS/SynBasic.pm
+++ AwfulCMS/SynBasic.pm
@@ -46,7 +46,7 @@ sub format {
   my $s=shift;
   my $string=shift;
 
-  $string=~s{\r}{\n}g;
+  $string=~s{\r}{}g;
   $string=~s{\n{2,}}{\n\n}g;
 
   # .*? -- non-greedy matching...
@@ -65,7 +65,7 @@ sub format {
   # ...get rid of newline between tags...
   $string=~s{>\n*<}{><}g;
   # ...and create all other paragraphs between double-newline or other <p>
-  $string=~s{(</p>|\n{2})([^<>]+?)(<.*?>|\n{2})}{$1<p>$2</p>$3}gsm;
+  $string=~s{(</p>|\n{2})([^<>]+?)(?=<.*?>|\n{2})}{$1<p>$2</p>}gsm;
   # now create the last paragraph, if needed...
   $string=~s{(</.*?>)([^<]*?)$}{$1<p>$2</p>}s;
   # ...and the first
