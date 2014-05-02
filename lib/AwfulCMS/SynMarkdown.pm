570bfa6024f6a7c7645fd6d3513b475f26542a2d
Bernd Wachter <bwachter-awfulcms@lart.info>
2014-05-02 19:45:05 +0300

    Add hacky SynBasic style anchor support

--- lib/AwfulCMS/SynMarkdown.pm
+++ lib/AwfulCMS/SynMarkdown.pm
@@ -6,6 +6,9 @@ This modules provides a parser for markdown formatted text.
 
 =cut
 
+# documentation for definition lists in extended markdown:
+# http://michelf.ca/projects/php-markdown/extra/#def-list
+
 use strict;
 use Text::Markdown 'markdown';
 #use Exporter 'import';
@@ -25,7 +28,23 @@ sub format {
   my $string=shift;
   my $vars=shift;
 
+
+
+
+  # [@...||@]
+  #$string=~s{\[\@([^<(]*?)\@\]\((.*?)\)}{<a name="$2" id="$2">$1</a>}g;
+
   my $html = markdown($string);
+
+  # allow anchor generation with mixed markdown/synBasic style:
+  # [@Description@](anchor name)
+  # It's easier to have markdown generate its links, and we pick off the ones
+  # that should be anchors later on
+  $html=~s{<a href="([^\"]*?)">@([^<]*?)@</a>}{<a name="$1" id="$1">$2</a>}g;
+
+  # process synBasic style anchor generation with [@..@]
+  # this needs checking added to make sure it's not happening inside of <pre></pre>
+  $html=~s{\[\@([^ @]*)(.*?)\@\]}{<a name="$1" id="$1">$1$2</a>}g;
 
   $html;
 }
